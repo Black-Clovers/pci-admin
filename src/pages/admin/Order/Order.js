6ba6edeed7f98c5ed7d0fdd772063f284fe3ff9e
dimanthathilakarathna98 <walahanduwathilakarathna@gmail.com>
2022-11-05 10:47:16 +0530

    changes

--- src/pages/admin/Order/Order.js
+++ src/pages/admin/Order/Order.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./PackageStyle.css";
+import "./order.css";
 import { useState, useEffect } from "react";
 
 
 
 function Order() {
   
-
+  const [client_DOB, setClient_DOB] = useState("");
 
 
   return (
@@ -29,7 +29,7 @@ function Order() {
                 <div className="col">
                   <input
                     type="text"
-                    value={package_name}
+                   
                     className="form-control"
                     placeholder="Package Name"
                     
@@ -39,30 +39,25 @@ function Order() {
               </div>
               <div className="row mt-4">
                 <div className="col">
-                  <select
-                    name="type"
-                    value={vehicle_type}
-                    className="form-select"
-                    aria-label="role" 
+                <div className="col">
+                  <input
+                    type="text"
                    
-                  >
-                    <option selected disabled value="0" >
-                      Vehicle Type
-                    </option>
-                    <option value="Car">Car</option>
-                    <option value="Van">Van</option>
-                    <option value="Mini Van">Mini van</option>
-                    <option value="Tuk Tuk">Tuk Tuk</option>
-                  </select>
-                  <p class="alert-txt">{formErrors.vehicle_type}</p>
+                    className="form-control"
+                    placeholder="Supplier Name"
+                    
+                  />
+                
+                </div>
+                
                 </div>
 
                 <div className="col">
                   <input
                     type="text"
-                    value={contacts}
+                   
                     className="form-control"
-                    placeholder="Contacts"
+                    placeholder="Reffernece Number"
                    
                   />
                 
@@ -73,7 +68,7 @@ function Order() {
                 <div className="col">
                   <input
                     type="text"
-                    value={mpassengers}
+                   
                     className="form-control"
                     placeholder="Maximum Passengers"
                     
@@ -83,7 +78,7 @@ function Order() {
                 <div className="col">
                   <input
                     type="text"
-                    value={price}
+                    
                     className="form-control"
                     placeholder="Price"
                     
@@ -91,19 +86,15 @@ function Order() {
              
                 </div>
                 <div className="col">
-                  <select
-                    name="status"
-                   
-                    className="form-select"
-                    aria-label="role"
-                    
-                  >
-                    <option selected disabled value="0">
-                      Status
-                    </option>
-                    <option value="Available">Available</option>
-                    <option value="Unavailable">Unavailable</option>
-                  </select>
+                <input name="dateOfBirth"
+                                           className="form-control"
+                                           placeholder="Date of Birth"
+                                           type="text"
+                                          
+                                           onFocus={(e) => e.target.type = 'date'} id="dateOfBirth" onChange={(e) => {
+                                        setClient_DOB(e.target.value)
+                                    }}/>
+                            
                 </div>
               </div>
               
@@ -112,51 +103,29 @@ function Order() {
                   <div class="form-group">
                     <textarea
                       class="form-control"
-                      value={package_overview}
+                     
                       id="exampleFormControlTextarea1"
                       rows="4"
-                      placeholder="Package Overview"
+                      placeholder="Delivery Address"
                      
                     ></textarea>
                   </div>
                 </div>
               </div>
-              <div className="row mt-4">
-                <div className="col">
-                  <div class="form-group">
-                    <textarea
-                      class="form-control"
-                    
-                      id="exampleFormControlTextarea1"
-                      rows="4"
-                      placeholder="Additional Info"
-                      
-                    ></textarea>
-                  </div>
-                </div>
-              </div>
+
 
               <div className="row mt-5">
                 <div className="d-flex justify-content-around align-items-center">
+                 
                   <button
                     type="submit"
                     id="reg"
                     className="btn btnRegister "
-                    onClick={handleSubmit}
+                   
                   >
                     Add Package
                   </button>
-                  <button
-                    type="button"
-                    id="edit"
-                    className="btn btnUpdate"
-                    onClick={sendPackage}
-                  >
-                    Update
-                  </button>
-                  <button type="button" id="delete" className="btn btnDelete" onClick={deletePackage}>
-                  Delete
-                  </button>
+                 
                 </div>
               </div>
             </form>
